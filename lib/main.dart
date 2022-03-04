d37ca38099a5bf6245519491374101cbbcd5af52
ChoiWheatley <chltmdgus604@naver.com>
2022-03-04 18:09:14 +0900

    당근마켓 숙제 정답 :: 바깥쪽 사각형부터 그리기. Flexible(혹은 Expanded) 사용하기. 정렬하기

--- lib/main.dart
+++ lib/main.dart
@@ -9,25 +9,45 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    /// Flexible: `flex` 값에 배수를 가지고 비율을 지정할 수 있다.
-    /// Expanded: `flex`를 1만큼 갖는다. 물론 따로 설정할 수도 있기는 하다.
+    /// 당근마켓 정답
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(),
-        body: Row(
-          children: [
-            Expanded(
-              child: Container(color: Colors.blue,),
+        home: Scaffold(
+          appBar: AppBar(),
+          body: Container(
+            height: 150,
+            padding: EdgeInsets.all(20),
+            child: Row(
+              children: [
+                Image.asset('assets/madeline.png', width: 150,),
+                /// 하트 아이콘을 오른쪽 정렬을 해도 제 자리에 있는 이유는 기본적으로 Column이나
+                /// Text나 모두 소심해서 그런 것이다. 그래서 width를 정의해야 한다.
+                /// SizedBox같은 걸 사용할 수도 있겠지만, 정적인 크기가 된다. 따라서 Flexible,
+                /// Expanded 등의 활용을 재고해 볼 수 있다.
+                Expanded(child:
+                Column(
+                  /// 정렬은 Text 컴포넌트 안에 있는 정렬기능을 쓸 수도 있고,
+                  /// Column, Row에 정의되어있는 정렬기능을 쓸 수도 있다.
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('마델린'),
+                    Text('Madeline'),
+                    Text('Celste'),
+                    Text('Madeline'),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: const [
+                        Icon(Icons.favorite),
+                        Text('4'),
+                      ],
+                    )
+                  ],
+                ),
+                ),
+              ],
             ),
-            Flexible(
-              child: Container(color: Colors.green,),flex: 5,
-            ),
-            Flexible(
-              child: Container(color: Colors.red,),flex: 5,
-            ),
-          ],
-        ),
-      )
+          ),
+        )
     );
   }
 }
