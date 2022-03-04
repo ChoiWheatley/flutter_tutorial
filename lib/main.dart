1456e2b762629fa62f359a64a89ff7d872dd3529
ChoiWheatley <chltmdgus604@naver.com>
2022-03-04 20:02:51 +0900

    state는 Widget 자체가 변할 경우 사용한다.
    StatefulWidget과 State<> 을 활용하여 상태를 갖는 위젯을 만들 수 있다. 위젯의 상태를 변경하고 렌더링을 다시 하게 만들고 싶다면 상태변경구문을 `setState((){})` 안에 콜백함수에 넣으면 된다.

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,28 @@
 import 'package:flutter/material.dart';
+// import 'package:flutter/services.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+/// StatefulWidget : state문법을 사용하면 자료를 보관할 수 있다.
+/// state는 안에 있는 값이 변한다. 값이 변하면 state를 사용하는 위젯이
+/// 자동으로 재 렌더링하게 된다. `setState()` 함수를 사용하면 된다.
+///
+/// 주로 자주 바뀌는 데이터들, 바뀌면 바로바로 보여주어야 하는 데이터들을
+/// state로 만들어주자.
+class MyApp extends StatefulWidget{
   MyApp({Key? key}) : super(key: key);
-  var a = 0;
+
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  State<MyApp> createState() => _MyAppState();
+}
+class _MyAppState extends State<MyApp> {
+  var a = 0;
+  final appBarName = "연락처앱";
+  final names = const["최승현", "김영숙", "홍길동", "피자집", "콩콩콩", "안녕!", "니아렁", "hello"];
+
+  @override Widget build(BuildContext context) { return MaterialApp(
       home: Scaffold(
         /// 우측 하단에 떠있는 버튼에 대한 프리셋이 있다.
         floatingActionButton: FloatingActionButton(
@@ -17,27 +30,29 @@ class MyApp extends StatelessWidget {
           child: Text(a.toString()),
           onPressed: (){
             /// 버튼 누르면 여기 코드를 실행해준다.
-            a++;
+            // a++;
+            /// state를 바꾸어주는 메서드를 호출한다.
+            setState(() {
+              a++;
+            });
           },
         ),
-        appBar: AppBar(),
+        appBar: AppBar(
+          title: Text(appBarName),
+        ),
         body: Container(
           /// ListView 사용하면 스크롤바 생김. 스크롤 위치 감시도 가능, 안보이는 위젯
           /// 메모리 절약
-          child: ListView.builder(itemCount: 100,
+          child: ListView.builder(
+            itemCount: names.length,
             itemBuilder: (buildContext, i){
               print('hello' + i.toString());
               // return MyListItem(name: "name_" + i.toString(),);
               /// ListTile을 쓰면 별도의 컴포넌트 만들 필요도 없이 바로 리스트에 필요한
               /// 아이템을 정의할 수 있다.
               return ListTile(
-                autofocus: true,
-                enabled: true,
-                hoverColor: Colors.amber,
-                focusColor: Colors.yellow,
-                selectedColor: Colors.yellowAccent,
                 leading: Icon(Icons.account_circle),
-                title: Text('홍길동' + i.toString()),
+                title: Text(names[i] + (a + i).toString()),
               );
             },
           ),
